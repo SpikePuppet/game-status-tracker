1d5c7ba2e5cd2411213e51b8369f79f85be9825d
Rhys Johns <rhysjohns@pooledenergy.com>
2021-08-09 22:35:05 +1000

    - Setup a simple database save of the current statuses

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/SpikePuppet/game-status-tracker
 
 go 1.16
 
-require github.com/gin-gonic/gin v1.7.3 // indirect
+require (
+	github.com/gin-gonic/gin v1.7.3 // indirect
+	github.com/lib/pq v1.10.2 // indirect
+)
--- main.go
+++ main.go
@@ -1,16 +1,28 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"net/http"
 	"time"
 
 	"github.com/gin-gonic/gin"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "go"
+	password = "gopassword"
+	dbname   = "gamestatustracker"
 )
 
 type GameStatus struct {
 	Title    string    `json:"title"`
 	DateTime time.Time `json:"datetime"`
+	Status   string    `json:"status"`
 }
 
 func main() {
@@ -29,8 +41,26 @@ func saveStatus(context *gin.Context) {
 
 	record.DateTime = time.Now()
 
-	fmt.Println(record.Title)
-	fmt.Println(record.DateTime)
+	psqlConnectionInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	db, err := sql.Open("postgres", psqlConnectionInfo)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	defer db.Close()
+
+	insertStatement := `
+	INSERT INTO gamestatus (Title, DateTime, Status)
+	VALUES ($1, $2, $3)`
+
+	_, err = db.Exec(insertStatement, record.Title, time.Now(), record.Status)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
 
 	context.JSON(200, gin.H{
 		"message": "Yep it works!",
