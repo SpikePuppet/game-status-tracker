5b03fd9c306604bd1bdcfe87fbe4f3e0f3bede77
Rhys Johns <rhysjohns@pooledenergy.com>
2021-08-09 22:17:22 +1000

    - Accept records

--- main.go
+++ main.go
@@ -1,6 +1,17 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+type GameStatus struct {
+	Title    string    `json:"title"`
+	DateTime time.Time `json:"datetime"`
+}
 
 func main() {
 	router := gin.Default()
@@ -9,6 +20,18 @@ func main() {
 }
 
 func saveStatus(context *gin.Context) {
+	var record GameStatus
+
+	if err := context.ShouldBindJSON(&record); err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	record.DateTime = time.Now()
+
+	fmt.Println(record.Title)
+	fmt.Println(record.DateTime)
+
 	context.JSON(200, gin.H{
 		"message": "Yep it works!",
 	})
